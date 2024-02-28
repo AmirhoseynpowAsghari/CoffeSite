843f9915171bcc83ef495bd23774b88a70111470
Amirhoseyn <146642878+AmirhoseynpowAsghari@users.noreply.github.com>
2024-02-28 00:23:45 -0800

    Update models.py

--- Product/models.py
+++ Product/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime
 from django.conf import settings
 
-# Create your models here.
+# Create your models here. 
 class Product(models.Model):
     name           = models.CharField(max_length=150)
     description    = models.TextField()
